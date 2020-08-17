524449bbbdaffcd341b3a67184e8234258c3e215
ta-khac-cong <luckysun.solutions@gmail.com>
2020-08-17 12:03:07 +1000

    Updates

---
+++ precache-manifest.0608d50f6d8ba6d30931a9fb632db0a7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd842c7bc4b3ce691049bbd0f24b44f2",
+    "url": "/deepcheck/index.html"
+  },
+  {
+    "revision": "0f447ed571dea9991e7b",
+    "url": "/deepcheck/static/css/2.614afd85.chunk.css"
+  },
+  {
+    "revision": "1bf93ed93fbcc75bbaa1",
+    "url": "/deepcheck/static/css/main.adfcd523.chunk.css"
+  },
+  {
+    "revision": "0f447ed571dea9991e7b",
+    "url": "/deepcheck/static/js/2.af11be3d.chunk.js"
+  },
+  {
+    "revision": "1bf93ed93fbcc75bbaa1",
+    "url": "/deepcheck/static/js/main.b8e5a83d.chunk.js"
+  },
+  {
+    "revision": "1d0c2bcbdba4f80acde7",
+    "url": "/deepcheck/static/js/runtime-main.dd8e4482.js"
+  },
+  {
+    "revision": "9081e0c35bdd8f1531a81e16900e0ca7",
+    "url": "/deepcheck/static/media/about-us.9081e0c3.png"
+  },
+  {
+    "revision": "ad81adcf5caa804c58964452b5b886d0",
+    "url": "/deepcheck/static/media/account-check.ad81adcf.png"
+  },
+  {
+    "revision": "5a8f804072f4b52a8fdbca37bd841c29",
+    "url": "/deepcheck/static/media/document-check.5a8f8040.png"
+  },
+  {
+    "revision": "cda72e5d9b916a09dc67b8cdc5f4ab6f",
+    "url": "/deepcheck/static/media/id-check.cda72e5d.png"
+  },
+  {
+    "revision": "76b8cacbe49bbd8d8f9d16de76301aea",
+    "url": "/deepcheck/static/media/img-service1.76b8cacb.png"
+  },
+  {
+    "revision": "7e511d7e2ff8dd4cb0ebda0bbf545a00",
+    "url": "/deepcheck/static/media/img-service2.7e511d7e.png"
+  },
+  {
+    "revision": "1b06d78ce8feebe2c0940d831985722c",
+    "url": "/deepcheck/static/media/img-service3.1b06d78c.png"
+  }
+]);
